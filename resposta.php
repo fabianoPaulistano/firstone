50e23811dbeaaa92b92b8e5e754c6936d650a7d9
Fabiano <fa79bs@gmail.com>
2021-01-17 10:36:24 -0300

    alterado o php

--- resposta.php
+++ resposta.php
@@ -3,6 +3,506 @@
 <body>
 <?php
 <p> Seu nome é e sua idade é </p>
+
+Porto Alegre
+Carlos Barbosa
+Ipiranga do Sul
+Três Arroios
+Lagoa dos Três Cantos
+Garibaldi
+Casca
+Nova Araçá
+Santa Maria
+Ivoti
+Horizontina
+Caxias do Sul
+Ijuí
+Nova Petrópolis
+Vista Alegre do Prata
+Bento Gonçalves
+Nova Bréscia
+Lajeado
+Sarandi
+Selbach
+Victor Graeff
+Farroupilha
+Passo Fundo
+Erechim
+Marau
+Santa Cruz do Sul
+Paraí
+Veranópolis
+Santo Ângelo
+Aratiba
+Santa Rosa
+Arroio do Meio
+Nova Boa Vista
+Três Passos
+São Marcos
+Novo Xingu
+Encantado
+Estrela
+Nova Prata
+Carazinho
+Santiago
+Capivari do Sul
+Não-Me-Toque
+Colinas
+Ibirubá
+Guaporé
+Espumoso
+Cerro Largo
+Montauri
+Gramado
+São Valentim do Sul
+Rondinha
+Imbé
+Santo Antônio do Palma
+Barra Funda
+São Pedro do Butiá
+Fagundes Varela
+São Domingos do Sul
+Boa Vista do Buricá
+Torres
+Saldanha Marinho
+David Canabarro
+Panambi
+Nova Pádua
+Vila Maria
+Serafina Corrêa
+Itaara
+Campinas do Sul
+Tapejara
+Frederico Westphalen
+Três de Maio
+Santo Antônio do Planalto
+Nova Candelária
+Picada Café
+Colorado
+Guabiju
+Antônio Prado
+Presidente Lucena
+Vista Gaúcha
+Cachoeirinha
+Estância Velha
+Vanini
+Dois Lajeados
+Chapada
+Quinze de Novembro
+Fortaleza dos Valos
+São José do Ouro
+Montenegro
+Flores da Cunha
+Constantina
+São Vendelino
+Esteio
+Salvador das Missões
+Ajuricaba
+Nicolau Vergueiro
+Estação
+Westfália
+Monte Belo do Sul
+Severiano de Almeida
+Sertão
+Osório
+Água Santa
+Cruz Alta
+Feliz
+Canoas
+Pareci Novo
+Linha Nova
+Harmonia
+São João do Polêsine
+Canela
+Barão
+Nova Bassano
+Sananduva
+São José do Inhacorá
+Alto Alegre
+Novo Hamburgo
+Tapera
+Teutônia
+Charqueadas
+Condor
+Tucunduva
+Bom Princípio
+Getúlio Vargas
+Coqueiros do Sul
+Santa Tereza
+Mato Leitão
+Muçum
+Capitão
+Bozano
+Campo Bom
+Nova Palma
+Rio Grande
+Coronel Barros
+Uruguaiana
+Poço das Antas
+Imigrante
+Capão da Canoa
+Dois Irmãos
+Augusto Pestana
+Morro Reuter
+Sobradinho
+Cachoeira do Sul
+Vila Flores
+Tio Hugo
+Silveira Martins
+Nova Ramada
+Nova Roma do Sul
+Cotiporã
+Pejuçara
+São Luiz Gonzaga
+Alegrete
+Bagé
+Nova Alvorada
+Arroio do Sal
+Anta Gorda
+Salvador do Sul
+Santa Clara do Sul
+Almirante Tamandaré do Sul
+Bom Retiro do Sul
+São Pedro da Serra
+Vista Alegre
+São Sebastião do Caí
+Carlos Gomes
+Catuípe
+Ibiaçá
+São Leopoldo
+Taquaruçu do Sul
+Santo Augusto
+Pelotas
+Humaitá
+Lagoa Vermelha
+Campina das Missões
+Santo Cristo
+Gaurama
+Vera Cruz
+Palmeira das Missões
+Guarani das Missões
+Vale Real
+São Borja
+Gravataí
+Camargo
+Xangri-Lá
+Nova Esperança do Sul
+Alto Feliz
+Barra do Guarita
+Taquari
+Triunfo
+Protásio Alves
+União da Serra
+Gentil
+São Jorge
+Santo Expedito do Sul
+Chiapetta
+Rodeio Bonito
+Relvado
+Soledade
+Boa Vista do Incra
+Guaíba
+Mariano Moro
+Ilópolis
+Cidreira
+Roca Sales
+Boa Vista do Sul
+Ipê
+Cândido Godói
+Tuparendi
+Taquara
+Coronel Pilar
+Sant'Ana do Livramento
+Mato Castelhano
+São Martinho
+Sapucaia do Sul
+Jacutinga
+Ponte Preta
+São José do Sul
+Pontão
+Miraguaí
+Santa Bárbara do Sul
+Santa Cecília do Sul
+Ivorá
+Ibiraiaras
+Marcelino Ramos
+Doutor Ricardo
+Ronda Alta
+Cruzeiro do Sul
+Bom Progresso
+Barra do Rio Azul
+Seberi
+Vespasiano Correa
+Porto Xavier
+Vacaria
+Igrejinha
+Giruá
+São Valentim
+Palmitinho
+Faxinal do Soturno
+Pinhal
+André da Rocha
+Tramandaí
+Ciríaco
+Barão de Cotegipe
+Cruzaltense
+Caibaté
+Nova Santa Rita
+Três Cachoeiras
+Tupandi
+Eldorado do Sul
+São José do Herval
+Santo Antônio da Patrulha
+Mato Queimado
+Viamão
+Engenho Velho
+Júlio de Castilhos
+Ernestina
+Pouso Novo
+Putinga
+Palmares do Sul
+Mormaço
+Glorinha
+Canudos do Vale
+Itaqui
+Portão
+Venâncio Aires
+Santa Vitória do Palmar
+Erebango
+Lindolfo Collor
+Sede Nova
+Jaguari
+Crissiumal
+Eugênio de Castro
+Morrinhos do Sul
+Sapiranga
+Paulo Bento
+Pinheirinho do Vale
+Três Coroas
+Barracão
+São Pedro do Sul
+Tupanciretã
+Dom Pedrito
+Campos Borges
+São Sepé
+Tenente Portela
+Derrubadas
+Jaguarão
+São José do Hortêncio
+Áurea
+Arroio do Tigre
+Coxilha
+Chuí
+Campestre da Serra
+Novo Barreiro
+Paim Filho
+Doutor Maurício Cardoso
+Vila Lângaro
+Quaraí
+Parobé
+Caçapava do Sul
+Boa Vista do Cadeado
+Caseiros
+Campo Novo
+Três Palmeiras
+Entre Rios do Sul
+Morro Redondo
+Muitos Capões
+Nonoai
+Viadutos
+Tabaí
+Travesseiro
+Mariana Pimentel
+Centenário
+Cacequi
+Ubiretama
+Boqueirão do Leão
+Lavras do Sul
+Brochier
+Rosário do Sul
+Alvorada
+São Gabriel
+Maximiliano de Almeida
+Caiçara
+Porto Mauá
+Candiota
+Passo do Sobrado
+Arroio dos Ratos
+Fazenda Vilanova
+Dona Francisca
+Camaquã
+Cambará do Sul
+Maratá
+Balneário Pinhal
+São Jerônimo
+Alegria
+Tapes
+Tupanci do Sul
+São João da Urtiga
+Arvorezinha
+Agudo
+Senador Salgado Filho
+Independência
+Porto Lucena
+Rio Pardo
+Coqueiro Baixo
+São Paulo das Missões
+Bossoroca
+Machadinho
+Arambaré
+Iraí
+Dom Pedro de Alcântara
+Porto Vera Cruz
+Muliterno
+Tiradentes do Sul
+Terra de Areia
+Sertão Santana
+Quatro Irmãos
+Nova Hartz
+Rolador
+Butiá
+Novo Cabrais
+Rolante
+Roque Gonzales
+Salto do Jacuí
+São Lourenço do Sul
+Herval
+Aceguá
+Marques de Souza
+Planalto
+Trindade do Sul
+Santo Antônio das Missões
+General Câmara
+Jóia
+São Vicente do Sul
+São Francisco de Paula
+Liberato Salzano
+Gramado dos Loureiros
+Erval Seco
+Maçambará
+Toropi
+Progresso
+Restinga Seca
+Sete de Setembro
+Paverama
+Forquetinha
+Formigueiro
+Maquiné
+Ametista do Sul
+Erval Grande
+Minas do Leão
+Itatiba do Sul
+Entre-Ijuís
+Esmeralda
+Araricá
+Estrela Velha
+Pinhal Grande
+Pedro Osório
+Sagrada Família
+Santa Maria do Herval
+Boa Vista das Missões
+Paraíso do Sul
+Novo Tiradentes
+São Francisco de Assis
+Braga
+Candelária
+Cerro Grande
+Inhacorá
+Capão do Cipó
+Alecrim
+Alpestre
+Sentinela do Sul
+Garruchos
+Lagoa Bonita do Sul
+Dois Irmãos das Missões
+Barra do Ribeiro
+Arroio do Padre
+Pirapó
+Itati
+São Miguel das Missões
+Faxinalzinho
+Bom Jesus
+Quevedos
+Coronel Bicaco
+Itapuca
+São Pedro das Missões
+Mostardas
+Floriano Peixoto
+São José dos Ausentes
+Santa Margarida do Sul
+Novo Machado
+Cacique Doble
+Barra do Quaraí
+Jacuizinho
+Três Forquilhas
+Vila Nova do Sul
+Esperança do Sul
+Pantano Grande
+Fontoura Xavier
+Pinheiro Machado
+Cerro Branco
+Riozinho
+Capela de Santana
+Cerro Grande do Sul
+Cristal do Sul
+Segredo
+Jaboticaba
+Piratini
+Arroio Grande
+Encruzilhada do Sul
+Tunas
+Itacurubi
+Tavares
+Rio dos Índios
+Mata
+Manoel Viana
+Vitória das Missões
+Dezesseis de Novembro
+São Martinho da Serra
+Sério
+Ibarama
+Caraá
+São José das Missões
+Pinhal da Serra
+Barros Cassal
+Monte Alegre dos Campos
+Canguçu
+Unistalda
+Cerrito
+Mampituba
+Dilermando de Aguiar
+Vale Verde
+São Nicolau
+Cristal
+Lagoão
+Hulha Negra
+São Valério do Sul
+Pedras Altas
+Ibirapuitã
+Vicente Dutra
+Capão Bonito do Sul
+Capão do Leão
+Gramado Xavier
+Santana da Boa Vista
+Jari
+Redentora
+Sinimbu
+Turuçu
+Amaral Ferrador
+Vale do Sol
+São José do Norte
+Passa Sete
+Charrua
+Benjamin Constant do Sul
+Herveiras
+Chuvisca
+Jaquirana
+Lajeado do Bugre
+Barão do Triunfo
+Dom Feliciano
+Pinto Bandeira
+
+
 ?>
 </body>
 </html>
